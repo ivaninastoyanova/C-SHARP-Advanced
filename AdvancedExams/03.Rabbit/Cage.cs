b4d848288f138d32de49fe13d2831b0ce682fece
ivaninastoyanova <72800115+ivaninastoyanova@users.noreply.github.com>
2020-10-28 23:21:22 +0200

    Reordering

---
+++ AdvancedExams/03.Rabbit/Cage.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace Rabbits
+{
+    public class Cage
+    {
+        private List<Rabbit> data;
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+        public int Count
+        {
+            get
+            {
+                return this.data.Count;
+            }
+        }
+
+        public Cage(string name, int capacity)
+        {
+            this.Name = name;
+            this.Capacity = capacity;
+            this.data = new List<Rabbit>();
+        }
+
+        public void Add(Rabbit rabbit)
+        {
+            if (this.Count < this.Capacity)
+                this.data.Add(rabbit);
+        }
+
+        public bool RemoveRabbit(string name)
+        {
+            if (this.data.Any(r => r.Name == name))
+            {
+                var current = this.data.Where(r => r.Name == name).FirstOrDefault();
+                this.data.Remove(current);
+                return true;
+            }
+
+            return false;
+        }
+        //•	Method RemoveSpecies(string species) - removes all rabbits by given species
+        public void RemoveSpecies(string species)
+        {
+           
+            this.data.RemoveAll(r => r.Species == species);
+
+        }
+        
+        public Rabbit SellRabbit(string name)
+        {
+            Rabbit rabbit = this.data.FirstOrDefault(r => r.Name == name);
+            for (int i = 0; i < this.Count; i++)
+            {
+                if(this.data[i].Name==name)
+                {
+                    this.data[i].Available = false;
+                }
+            }
+           
+            return rabbit;
+        }
+
+        //•	Method SellRabbitsBySpecies(string species) - sells(set their Available property to false without removing them from the collection) and returns all rabbits from that species as an array
+        public Rabbit[] SellRabbitsBySpecies(string species)
+        {
+            var rabbits = new List<Rabbit>();
+            //rabbits = this.data.Where(x => x.Species == species).ToList();
+            //rabbits.ForEach(x => x.Available = false);
+            foreach (var item in this.data)
+            {
+                if (item.Species == species)
+                {
+                    item.Available = false;
+                    rabbits.Add(item);
+                }
+            }
+
+            return rabbits.ToArray();
+
+        }
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Rabbits available at {this.Name}:");
+            foreach (var item in this.data.Where(r=>r.Available==true))
+            {
+                sb.AppendLine(item.ToString());
+            }
+            return sb.ToString().Trim();
+        }
+    }
+}
---
+++ AdvancedExams/03.Rabbit/Rabbit.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rabbits
+{
+   public class Rabbit
+    {
+        public string Name { get; set; }
+        public string Species { get; set; }
+        public bool Available { get; set; }
+        public Rabbit(string name, string species)
+        {
+            this.Name = name;
+            this.Species = species;
+            this.Available = true;
+        }
+        public override string ToString()
+        {
+            return $"Rabbit ({this.Species}): {this.Name}".ToString();
+        }
+    }
+}
